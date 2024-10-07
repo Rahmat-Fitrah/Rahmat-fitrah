0fc05e5005b5db03dae2c231902c93778db4ce27
Rahmat-Fitrah <rahmatfitrah125@gmail.com>
2024-10-07 15:11:36 +0700

    Add files via upload

--- Discount.java
+++ Discount.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
-
 public class Discount {
 
     public static void main (String[] args) throws Exception{
-        Double totalBayar;
+        Double totalBelanja, totalBayar, discount;
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukan Total Belanja");
-        totalBayar = sc.nextDouble();
+        totalBelanja = sc.nextDouble();
 
-        if (totalBayar > 700000) {
-            totalBayar -= 30000;
-        }
-        System.out.println("Total yang harus dibayar: "+totalBayar);
+        if (totalBelanja > 50000) {
+            discount =  5.0/100*totalBelanja;
+        }else if (totalBelanja > 100000) {
+            discount =  10/100*totalBelanja;
+        }else if (totalBelanja > 200000) {
+            discount =  15/100*totalBelanja;
+        }else
+            discount = 0.0;
+        
+        totalBayar = totalBelanja-discount;
+        System.out.println("Total Belanja: "+totalBelanja);
+        System.out.println("Discount: "+discount);
+        System.out.println("Total dibayar: "+totalBayar);
         sc.close();
     }
     
