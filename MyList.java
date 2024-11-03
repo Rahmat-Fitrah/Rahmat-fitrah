c516cd99571ef2554b5727e70ce2d027e899289a
Rahmat-Fitrah <rahmatfitrah125@gmail.com>
2024-11-03 16:50:17 +0700

    Add files via upload

---
+++ MyList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class MyList {
+    public static void main(String[] args) {
+        ArrayList Nama_Barang = new ArrayList();
+        Nama_Barang.add("Laptop");
+        Nama_Barang.add("TV");
+        Nama_Barang.add("Mesin Cuci");
+        Nama_Barang.add("Kulkas");
+        System.out.println(Nama_Barang);
+    }
+}
