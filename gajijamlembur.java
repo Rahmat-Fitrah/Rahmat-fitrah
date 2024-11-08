841d9f1c19df4fb23dbf173fb43d9345948767ad
Rahmat-Fitrah <rahmatfitrah125@gmail.com>
2024-11-08 13:30:33 +0700

    Add files via upload

--- gajijamlembur.java
+++ gajijamlembur.java
@@ -6,7 +6,7 @@ public class gajijamlembur {
         int GajiBulannan  = scanner.nextInt();
         System.out.print("Masukan Jam Lembur : ");  
         int JamLembur  = scanner.nextInt();
-        System.out.print("Masukan Gaji Perjam : ");  
+        System.out.print("Masukan Gaji Jam Lembur : ");  
         int GajiPerjam  = scanner.nextInt();
         if (GajiPerjam > 0) {
             int GajiPerkerja = GajiPerjam * JamLembur + GajiBulannan  ;
