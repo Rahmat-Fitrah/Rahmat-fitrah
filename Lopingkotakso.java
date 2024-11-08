da0c3a3dbcd3eed07bae09328d0aaa2d15ff53ed
Rahmat-Fitrah <rahmatfitrah125@gmail.com>
2024-11-08 14:00:00 +0700

    Add files via upload

--- Lopingkotakso.java
+++ Lopingkotakso.java
@@ -2,8 +2,8 @@ public class Lopingkotakso {
     public static void main(String[] args) {
 
         for (int a = 0; a < 4; a++) {
-            for (int b = 0; b < 5; b++) { 
-                if (b <5-a) {
+            for (int b = 0; b < 4; b++) { 
+                if (b <3-a) {
                     System.out.print("S");
                 } else {
                     System.out.print("O");
